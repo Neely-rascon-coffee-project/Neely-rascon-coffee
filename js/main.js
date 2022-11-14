d7350b2ed6349be5db6549cfaa268f944ff1eb48
Andrew-Neely-82 <114946953+Andrew-Neely-82@users.noreply.github.com>
2022-11-14 14:50:49 -0600

    updated

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 (() => {
     "use strict";
     let coffees = [
-        { id: 1, name: "Light City", roast: "light" },
-        { id: 2, name: "Half City", roast: "light" },
-        { id: 3, name: "Cinnamon", roast: "light" },
-        { id: 4, name: "City", roast: "medium" },
-        { id: 5, name: "American", roast: "medium" },
-        { id: 6, name: "Breakfast", roast: "medium" },
-        { id: 7, name: "High", roast: "dark" },
-        { id: 8, name: "Continental", roast: "dark" },
-        { id: 9, name: "New Orleans", roast: "dark" },
-        { id: 10, name: "European", roast: "dark" },
-        { id: 11, name: "Espresso", roast: "dark" },
-        { id: 12, name: "Viennese", roast: "dark" },
-        { id: 13, name: "Italian", roast: "dark" },
-        { id: 14, name: "French", roast: "dark" },
+        {id: 1, name: "Light City", roast: "light"},
+        {id: 2, name: "Half City", roast: "light"},
+        {id: 3, name: "Cinnamon", roast: "light"},
+        {id: 4, name: "City", roast: "medium"},
+        {id: 5, name: "American", roast: "medium"},
+        {id: 6, name: "Breakfast", roast: "medium"},
+        {id: 7, name: "High", roast: "dark"},
+        {id: 8, name: "Continental", roast: "dark"},
+        {id: 9, name: "New Orleans", roast: "dark"},
+        {id: 10, name: "European", roast: "dark"},
+        {id: 11, name: "Espresso", roast: "dark"},
+        {id: 12, name: "Viennese", roast: "dark"},
+        {id: 13, name: "Italian", roast: "dark"},
+        {id: 14, name: "French", roast: "dark"},
     ];
 
     let lightRoast = coffees.filter((coffee) => {
@@ -29,22 +29,16 @@
         return coffee.roast === "dark";
     });
 
-    let tbody = document.querySelector("#coffees");
-    tbody.innerHTML = renderCoffees(coffees);
-
-    let submitButton = document.querySelector("#submit");
-    submitButton.addEventListener("click", updateCoffees);
-
-    let roastSearch = document.getElementById("form-control");
-    roastSearch.addEventListener("keyup", searchCoffees);
-
-    let roastSelection = document.getElementById("roast-selection");
-    roastSelection.addEventListener("change", updateCoffees);
-
-    let inputName = document.querySelector("#input-name");
-    let inputRoast = document.querySelector("#input-roast");
     let addCoffeeButton = document.querySelector("#input-submit");
     addCoffeeButton.addEventListener("click", addCoffees);
+    let inputName = document.querySelector("#input-name");
+    let inputRoast = document.querySelector("#input-roast");
+    let roastSelection = document.getElementById("roast-selection");
+    roastSelection.addEventListener("change", updateCoffees);
+    let submitButton = document.querySelector("#submit");
+    submitButton.addEventListener("click", updateCoffees);
+    let tbody = document.querySelector("#coffees");
+    tbody.innerHTML = renderCoffees(coffees);
 
     // Search functionality
     let searchBox = document.getElementById("searchBox");
@@ -85,7 +79,7 @@
         let addID = coffees.length + 1;
         let addName = inputName.value.toString();
         let addRoast = inputRoast.value.toString();
-        input = { id: addID, name: addName, roast: addRoast };
+        input = {id: addID, name: addName, roast: addRoast};
         coffees.push(input);
         tbody.innerHTML = renderCoffees(coffees);
     }
